75fe737315cb9876f839d7533734775443a20b06
Amber Taranto <ambzzzz@gmail.com>
2019-09-10 03:00:19 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,17 @@
 class Pokemon
+    attr_accessor :name, :type, :db, :hp
+    attr_reader :id
+
+    def initialize(keyword)
+
+    end
+
+    def self.save(name, type, db)
+        db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+    end
+
+    def self.find(id, db)
+        pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
+
+    end
 end
